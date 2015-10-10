f6263f4e13fd2c0b00ba68275a562243e0381818
melvincabatuan <melvincabatuan@gmail.com>
2015-10-10 16:29:27 +0800

    ScreenSlidesDemo Submission

---
+++ app/src/main/java/com/cabatuan/screenslidesdemo/MainActivity.java
@@ -0,0 +1,54 @@
+package com.cabatuan.screenslidesdemo;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends ListActivity {
+
+    private static Sample[] mSamples;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Instantiate the list of samples.
+        mSamples = new Sample[]{
+                new Sample(R.string.title_screen_slide, ScreenSlideActivity.class),
+        };
+
+        setListAdapter(new ArrayAdapter<Sample>(this, R.layout.my_list_item,
+                android.R.id.text1,
+                mSamples));
+    }
+
+    @Override
+    protected void onListItemClick(ListView listView, View view, int position, long id) {
+        // Launch the sample associated with this list position.
+        startActivity(new Intent(MainActivity.this, mSamples[position].activityClass));
+    }
+
+    /**
+     * This class describes an individual sample (the sample title, and the activity class that
+     * demonstrates this sample).
+     */
+    private class Sample {
+        private CharSequence title;
+        private Class<? extends Activity> activityClass;
+
+        public Sample(int titleResId, Class<? extends Activity> activityClass) {
+            this.activityClass = activityClass;
+            this.title = getResources().getString(titleResId);
+        }
+
+        @Override
+        public String toString() {
+            return title.toString();
+        }
+    }
+}
